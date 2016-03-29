392fa22d79f728b5064df8878473008ec9b8d651
Danny Willems <contact@danny-willems.be>
2016-03-29 17:40:01 +0200

    update to gen_js_api version

--- README.md
+++ README.md
@@ -18,6 +18,19 @@ This plugin works with Cordova 3.x, 4.x and 5.x version.
 
 Source: [cordova-sms-plugin](https://github.com/cordova-sms/cordova-sms-plugin)
 
+## Repository branches and tags
+
+We are migrating bindings from
+[js_of_ocaml](https://github.com/ocsigen/js_of_ocaml) (low level bindings) to
+[gen_js_api](https://github.com/lexifi/gen_js_api) (high level bindings).
+
+The gen_js_api binding allows to use *pure* ocaml types (you don't have to use
+the ## syntax from js_of_ocaml or Js.string type but only # and string type).
+
+The js_of_ocaml version is available in the branch
+[*js_of_ocaml*](https://github.com/dannywillems/ocaml-cordova-plugin-sms/tree/js_of_ocaml)
+but we **recommend** to use the gen_js_api version which is the master branch.
+
 ## How to use ?
 
 See the official documentation
@@ -33,13 +46,13 @@ documentation on js_of_ocaml). If we did, *sms* will be set to **undefined**
 because the *sms* javascript object doesn't exist when we create
 the variable.
 
-Instead, we provide a function *Sms.sms* of type unit -> Sms.sms
-Js.t which does the binding when you call it.
+Instead, we provide a function *Sms.t* of type unit -> Sms.sms
+which does the binding when you call it.
 So, use
 
 ```OCaml
 let on_device_ready =
-  let sms = Sms.sms () in
+  let sms = Sms.t () in
   (* Some code *)
 
 let _ =
--- sms.mli
+++ sms.mli
@@ -1,44 +1,78 @@
 (* --------------------------------------------------------------------------- *)
-type android_options =
-  <
-    intent            : Js.js_string Js.t Js.readonly_prop
-  > Js.t
+class android_options : Ojs.t ->
+  object
+    inherit Ojs.obj
+    method intent            : string
+  end
 
-type android_intent
+type android_intent =
+  | No_open [@js ""]
+  | Intent  [@js "Intent"]
+  [@@js.enum]
+
+[@@@js.stop]
 val no_open : android_intent
 val intent  : android_intent
+[@@@js.start]
 
-val create_android_options : ?intent:android_intent -> unit -> android_options
+[@@@js.implem
+let no_open         = No_open
+let intent          = Intent
+]
+
+val create_android_options :
+  ?intent:(android_intent [@js.default No_open])  ->
+  unit                                    ->
+  android_options
+[@@js.builder]
 (* --------------------------------------------------------------------------- *)
 
 (* --------------------------------------------------------------------------- *)
-type options =
-  <
-    replaceLineBreaks : bool Js.t Js.readonly_prop ;
-    android           : android_options Js.readonly_prop
-  > Js.t
-
-val create_options :  ?replace_line_breaks:bool ->
-                      ?android:android_options option ->
-                      unit -> options
-(* --------------------------------------------------------------------------- *)
-
-(* --------------------------------------------------------------------------- *)
-class type sms =
+class options : Ojs.t ->
   object
+    inherit Ojs.obj
+    method replace_line_breaks  : bool
+    method android              : android_options
+  end
+
+val create_options :
+  ?replace_line_breaks:(bool [@js.default false]) ->
+  ?android:(android_options option [@js.default None])   ->
+  unit                                            ->
+  options
+[@@js.builder]
+(* --------------------------------------------------------------------------- *)
+
+(* --------------------------------------------------------------------------- *)
+class sms : Ojs.t ->
+  object
+    inherit Ojs.obj
     (* send [number] [message] *)
-    method send       : Js.js_string Js.t -> Js.js_string Js.t -> unit Js.meth
+    method send       : string -> (* number *)
+                        string -> (* message *)
+                        unit
     (* send [number] [message] [options] *)
-    method send_opt   : Js.js_string Js.t -> Js.js_string Js.t -> options
-                      -> unit Js.meth
-    (* send [number] [message] [options] [succes_callback *)
-    method send_succ  : Js.js_string Js.t -> Js.js_string Js.t -> options
-                      -> (unit -> unit) -> unit Js.meth
-    (* send [number] [message] [options] [succes_callback [error_callback] *)
-    method send_cb    : Js.js_string Js.t -> Js.js_string Js.t -> options
-                      -> (unit -> unit) -> (Js.js_string Js.t -> unit)
-                      -> unit Js.meth
+    method send_opt   : string  -> (* number *)
+                        string  -> (* message *)
+                        options -> (* options *)
+                        unit
+    (* send [number] [message] [options] [success_callback *)
+    method send_succ  : string          -> (* number *)
+                        string          -> (* message *)
+                        options         -> (* options *)
+                        (unit -> unit)  -> (* success_cb *)
+                        unit
+    (* send [number] [message] [options] [success_callback [error_callback] *)
+    method send_cb    : string              -> (* number *)
+                        string              -> (* message *)
+                        options             -> (* options *)
+                        (unit -> unit)      -> (* success_cb *)
+                        (string -> unit)    -> (* error_cb *)
+                        unit
   end
 (* --------------------------------------------------------------------------- *)
 
-val sms : unit -> sms Js.t
+(* --------------------------------------------------------------------------- *)
+val t : unit -> sms
+[@@js.get "sms"]
+(* --------------------------------------------------------------------------- *)
